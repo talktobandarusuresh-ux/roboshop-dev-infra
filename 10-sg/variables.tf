f5764edc18e174f0207d8083223c25e4e92ce812
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-01-10 20:14:35 +0530

    kubernetes

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -1,16 +1,25 @@
 variable "project_name" {
   default = "roboshop"
 }
+
 variable "environment" {
   default = "dev"
 }
+
 variable "sg_names" {
   default = [
-    #databases
+    # databases
     "mongodb", "redis", "mysql", "rabbitmq",
-    #backend
+    # backend
     "catalogue", "user", "cart", "shipping", "payment",
-    #frontend
-  "frontend"]
+    # frontend
+    "frontend",
+    # bastion
+    "bastion",
+    # frontend load balancer
+    "frontend_alb",
+    # Backend ALB
+    "backend_alb",
+    "open_vpn"
+  ]
 }
-
