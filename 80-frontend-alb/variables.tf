adbb9705650ac686a0025e5aee4d71fd7ee8b8d2
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-01-15 13:03:34 +0530

    Terraform

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "domain_name" {
+  default = "bandarusuresh.in"
+}
+
+variable "zone_id" {
+  default = "Z00483002KQGHYINL2NKX"
+}
